e8fd8754e612cbd0864d9deec4325d88debbb5e4
TERRYYYY <kariuki.terry11@gmail.com>
2023-06-19 07:29:49 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -73,54 +73,6 @@ const App = () => (
     </main>
   </div>
 );
-
-export default class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      selectionConfiguration: {
-        type: CALENDAR_SELECTION_TYPE.single,
-        date: null,
-      },
-    };
-  }
-
-  handleDateSelect = (date) => {
-    this.setState({
-      selectionConfiguration: {
-        type: this.props.selectionConfiguration.type,
-        date: date,
-      },
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <BpkInput
-          id="dateInput"
-          type={INPUT_TYPES.text}
-          name="date"
-          value={(this.state.selectionConfiguration.date || '').toString()}
-          placeholder="Departure date"
-        />
-        <BpkCalendar
-          id="calendar"
-          onDateSelect={this.handleDateSelect}
-          formatMonth={formatMonth}
-          formatDateFull={formatDateFull}
-          daysOfWeek={daysOfWeek}
-          weekStartsOn={1}
-          changeMonthLabel="Change month"
-          nextMonthLabel="Next month"
-          previousMonthLabel="Previous month"
-          selectionConfiguration={this.state.selectionConfiguration}
-        />
-      </div>
-    );
-  }
-}
 // export default class Application {
 //   constructor() {
 
